6bbb310ddaf1a92ec23772bc9d066d8ffdabb1be
Stephen <stephenfauvergue@gmail.com>
2019-03-15 10:36:30 +0100

    add Observable and Subject

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,37 @@
-import {Component} from '@angular/core';
-
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Observable, Subscription} from 'rxjs';
+import {interval} from 'rxjs';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss']
 })
-export class AppComponent  {
-    constructor() {}
+export class AppComponent implements OnInit, OnDestroy {
+    constructor() {
+    }
 
+
+    secondes: number;
+    counterSubscription: Subscription;
+
+    ngOnInit() {
+        // pas Observable.interbal ?
+        const counter = interval(1000);
+        this.counterSubscription = counter.subscribe(
+            (value) => {
+                this.secondes = value;
+            },
+            (error) => {
+                console.log('Uh-oh, an error occurred! : ' + error);
+            },
+            () => {
+                console.log('Observable complete!');
+            }
+        );
+    }
+
+    ngOnDestroy() {
+        this.counterSubscription.unsubscribe();
+    }
 }
--- src/app/appareil-view/appareil-view.component.ts
+++ src/app/appareil-view/appareil-view.component.ts
@@ -1,14 +1,16 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {AppareilService} from '../services/appareil.service';
+import {Subscription} from 'rxjs';
 
 @Component({
     selector: 'app-appareil-view',
     templateUrl: './appareil-view.component.html',
     styleUrls: ['./appareil-view.component.scss']
 })
-export class AppareilViewComponent implements OnInit {
+export class AppareilViewComponent implements OnInit, OnDestroy {
 
     appareils: any[];
+    appareilSubscription: Subscription;
     isAuth: boolean = false;
 
     lastUpdate = new Promise((resolve, reject) => {
@@ -24,7 +26,12 @@ export class AppareilViewComponent implements OnInit {
     }
 
     ngOnInit() {
-        this.appareils = this.appareilService.appareils;
+        this.appareilSubscription = this.appareilService.appareilsSubject.subscribe(
+            (appareils: any[]) => {
+                this.appareils = appareils;
+            }
+        );
+        this.appareilService.emitAppareilSubject();
     }
 
     onAllumer() {
@@ -37,6 +44,10 @@ export class AppareilViewComponent implements OnInit {
         } else {
             return null;
         }
+    }
+
+    ngOnDestroy() {
+        this.appareilSubscription.unsubscribe();
     }
 
 }
