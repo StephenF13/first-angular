50498111a92ec9714f8b62fe2207089ab09d8f8f
Stephen <stephenfauvergue@gmail.com>
2019-03-20 16:53:58 +0100

    create form to add User (reactive method)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,51 +1,60 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { MonPremierComponent } from './mon-premier/mon-premier.component';
-import { AppareilComponent } from './appareil/appareil.component';
-import { FormsModule } from '@angular/forms';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {MonPremierComponent} from './mon-premier/mon-premier.component';
+import {AppareilComponent} from './appareil/appareil.component';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
-import { AppareilService } from './services/appareil.service';
-import { AuthComponent } from './auth/auth.component';
-import { AppareilViewComponent } from './appareil-view/appareil-view.component';
+import {AppareilService} from './services/appareil.service';
+import {AuthComponent} from './auth/auth.component';
+import {AppareilViewComponent} from './appareil-view/appareil-view.component';
 import {RouterModule, Routes} from '@angular/router';
 import {AuthService} from './services/auth.service';
-import { SingleAppareilComponent } from './single-appareil/single-appareil.component';
-import { FourOhFourComponent } from './four-oh-four/four-oh-four.component';
+import {SingleAppareilComponent} from './single-appareil/single-appareil.component';
+import {FourOhFourComponent} from './four-oh-four/four-oh-four.component';
 import {AuthGuard} from './services/auth-guard.service';
-import { EditAppareilComponent } from './edit-appareil/edit-appareil.component';
+import {EditAppareilComponent} from './edit-appareil/edit-appareil.component';
+import {UserService} from './services/user.service';
+import {UserListComponent} from './user-list/user-list.component';
+import {NewUserComponent} from './new-user/new-user.component';
 
 const appRoutes: Routes = [
-    { path: 'appareils', canActivate: [AuthGuard], component: AppareilViewComponent },
-    { path: 'appareils/:id', canActivate: [AuthGuard], component: SingleAppareilComponent },
-    { path: 'edit', canActivate: [AuthGuard], component: EditAppareilComponent },
-    { path: 'auth', component: AuthComponent },
-    { path: '', component: AppareilViewComponent },
-    { path: 'not-found', component: FourOhFourComponent },
-    { path: '**', redirectTo: 'not-found' }
+    {path: 'appareils', canActivate: [AuthGuard], component: AppareilViewComponent},
+    {path: 'appareils/:id', canActivate: [AuthGuard], component: SingleAppareilComponent},
+    {path: 'edit', canActivate: [AuthGuard], component: EditAppareilComponent},
+    {path: 'users', component: UserListComponent},
+    {path: 'new-user', component: NewUserComponent},
+    {path: 'auth', component: AuthComponent},
+    {path: '', component: AppareilViewComponent},
+    {path: 'not-found', component: FourOhFourComponent},
+    {path: '**', redirectTo: 'not-found'}
 ];
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    MonPremierComponent,
-    AppareilComponent,
-    AuthComponent,
-    AppareilViewComponent,
-    SingleAppareilComponent,
-    FourOhFourComponent,
-    EditAppareilComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    FormsModule,
-    RouterModule.forRoot(appRoutes)
+    declarations: [
+        AppComponent,
+        MonPremierComponent,
+        AppareilComponent,
+        AuthComponent,
+        AppareilViewComponent,
+        SingleAppareilComponent,
+        FourOhFourComponent,
+        EditAppareilComponent,
+        UserListComponent,
+        NewUserComponent
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        FormsModule,
+        ReactiveFormsModule,
+        RouterModule.forRoot(appRoutes)
 
-  ],
-  providers: [AppareilService, AuthService, AuthGuard],
-  bootstrap: [AppComponent]
+    ],
+    providers: [AppareilService, AuthService, AuthGuard, UserService],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
